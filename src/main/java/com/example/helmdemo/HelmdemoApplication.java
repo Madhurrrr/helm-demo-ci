7bd2bdb807255f929d0bbbc9947213693e448237
Madhur <86947947+Madhurrrr@users.noreply.github.com>
2024-09-05 11:57:23 +0530

    Code push

---
+++ src/main/java/com/example/helmdemo/HelmdemoApplication.java
@@ -0,0 +1,26 @@
+package com.example.helmdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class HelmdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelmdemoApplication.class, args);
+	}
+
+	@RestController
+	@RequestMapping("/api")
+	public class HelmdemoController {
+
+		@GetMapping("/hello")
+		public String sayHello() {
+			return "Hello World from helm !";
+		}
+	}
+
+}
